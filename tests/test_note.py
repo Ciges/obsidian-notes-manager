9c4e90945d19052c710b69aadebc34206ffab259
Ciges <jmanuel.ciges@mail.com>
2025-05-29 12:43:05 +0200

    Añadidos tests para la clase Note

---
+++ tests/test_note.py
@@ -0,0 +1,26 @@
+import unittest
+from classes.note import Note
+
+class TestNote(unittest.TestCase):
+    def test_get_content_from_path(self):
+        try:
+            Note.get_content_from_path("BANDEJA DE ENTRADA")
+        except FileNotFoundError:
+            self.skipTest("BANDEJA DE ENTRADA note file not found.")
+
+    def test_get_content_with_instance_and_path(self):
+        note = Note()
+        try:
+            note.get_content("BANDEJA DE ENTRADA")
+        except FileNotFoundError:
+            self.skipTest("BANDEJA DE ENTRADA note file not found.")
+
+    def test_get_content_with_instance(self):
+        note = Note("BANDEJA DE ENTRADA")
+        try:
+            note.get_content()
+        except FileNotFoundError:
+            self.skipTest("BANDEJA DE ENTRADA note file not found.")
+
+if __name__ == "__main__":
+    unittest.main()
